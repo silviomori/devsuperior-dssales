7fcf8cc9cb48335f855c1c1af5eb56a349666483
Silvio Mori Neto <silviomori@gmail.com>
2022-02-08 15:16:00 -0800

    Create sales-summary-card skeleton

--- front-web/src/components/sales-summary/sales-summary-card.tsx
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -1,7 +1,14 @@
 import './styles.css';
+import { ReactComponent as CardIcon } from '../../assets/sales-summary-average.svg';
 
 function SalesSummaryCard() {
-  return <div className="base-card sales-summary-card"></div>;
+  return (
+    <div className="base-card sales-summary-card">
+      <CardIcon />
+      <h3 className="sales-summary-card-value">534,00</h3>
+      <span className="sales-summary-card-label">Average</span>
+    </div>
+  );
 }
 
 export default SalesSummaryCard;
