196cab6f7918ecae592c5a331d1c32c68c011be5
Silvio Mori Neto <silviomori@gmail.com>
2022-02-08 14:50:18 -0800

    Create sales-summary component skeleton

---
+++ front-web/src/components/sales-summary/index.tsx
@@ -0,0 +1,15 @@
+import SalesSummaryCard from './sales-summary-card';
+import './styles.css';
+
+function SalesSummary() {
+  return (
+    <div className="sales-summary-container">
+      <SalesSummaryCard />
+      <SalesSummaryCard />
+      <SalesSummaryCard />
+      <SalesSummaryCard />
+    </div>
+  );
+}
+
+export default SalesSummary;
---
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+function SalesSummaryCard() {
+  return <div className="base-card sales-summary-card"></div>;
+}
+
+export default SalesSummaryCard;
