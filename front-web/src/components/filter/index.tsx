deb42191a59566e82af5edce0e51ed882c8a1860
Silvio Mori Neto <silviomori@gmail.com>
2022-02-08 05:23:43 -0800

    Include date and gender fields in the filter component

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,9 +1,26 @@
 import './styles.css';
 
+import FlatPicker from 'react-flatpickr';
+import 'flatpickr/dist/themes/material_blue.css';
+
 function Filter() {
+  const onChangeDate = (dates: Date[]) => {
+    console.log(dates);
+  };
   return (
     <div className="filter-container base-card">
-      <h1>Filter</h1>
+      <FlatPicker
+        options={{ mode: 'range', dateFormat: 'm/d/Y', showMonths: 2 }}
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Select period"
+      />
+      <select className="filter-input">
+        <option value="">Select gender</option>
+        <option value="MALE">Male</option>
+        <option value="FEMALE">Female</option>
+        <option value="OTHER">Other</option>
+      </select>
     </div>
   );
 }
